6b427da7262c3e5c992e1a3d761ef4103123c4df
CombinE88 <Roland.Zipper@web.de>
2018-03-01 17:42:40 +0100

    sequence reorded to prevent batch breaking

--- OpenRA.Mods.MW/Traits/Undead Faction/AcolytePreyBuildInfo.cs
+++ OpenRA.Mods.MW/Traits/Undead Faction/AcolytePreyBuildInfo.cs
@@ -105,7 +105,7 @@ namespace OpenRA.Mods.MW.Traits
         class PreyOrderBuildTargeter : UnitOrderTargeter
         {
             public PreyOrderBuildTargeter(string cursor)
-                : base("PreyBuild", 6, cursor, true, false) { }
+                : base("PreyBuild", 6, cursor, false, true) { }
 
             public override bool CanTargetActor(Actor self, Actor target, TargetModifiers modifiers, ref string cursor)
             {
